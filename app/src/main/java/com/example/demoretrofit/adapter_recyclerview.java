8463976e962f56f1e0dbd41054ed76f93fb3a45b
Muhammad Usman <517459@muni.cz>
2021-08-21 22:44:17 +0100

    added imageview

--- app/src/main/java/com/example/demoretrofit/adapter_recyclerview.java
+++ app/src/main/java/com/example/demoretrofit/adapter_recyclerview.java
@@ -1,20 +1,26 @@
 package com.example.demoretrofit;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 public class adapter_recyclerview extends RecyclerView.Adapter<adapter_recyclerview.myviewHolder> {
     List<model> data;
 
+
     public adapter_recyclerview(List<model> data) {
         this.data = data;
+
     }
 
     @NonNull
@@ -30,6 +36,8 @@ public class adapter_recyclerview extends RecyclerView.Adapter<adapter_recyclerv
     @Override
     public void onBindViewHolder(@NonNull myviewHolder holder, int position) {
         holder.text.setText(data.get(position).getCourseName());
+        Glide.with(holder.text.getContext()).load(data.get(position).getCourseimg()).into(holder.img);
+
     }
 
     @Override
@@ -41,12 +49,13 @@ public class adapter_recyclerview extends RecyclerView.Adapter<adapter_recyclerv
 
 {
 TextView text;
+ImageView img;
 
 
     public myviewHolder(@NonNull View itemView) {
         super(itemView);
         text=itemView.findViewById(R.id.texte);
-
+        img=itemView.findViewById(R.id.imag);
     }
 }
 }
