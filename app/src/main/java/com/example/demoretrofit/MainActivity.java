cb492fd0fd826615c4453a526570a1466d31c37e
Muhammad Usman <517459@muni.cz>
2021-08-22 14:51:41 +0100

    Porgress bar implementation

--- app/src/main/java/com/example/demoretrofit/MainActivity.java
+++ app/src/main/java/com/example/demoretrofit/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -18,7 +19,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+        ProgressDialog progressDialog;
     RecyclerView recyclerView;
     List<model> imagesList;
     @Override
@@ -39,6 +40,10 @@ showdata();
     public void showdata()
 
     {
+        progressDialog=new ProgressDialog(this);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.show();
         myapi webService=myapi.retrofit.create(myapi.class);
         Call<List<model>> call=webService.getPosts();
         call.enqueue(new Callback<List<model>>() {
@@ -46,6 +51,7 @@ showdata();
             public void onResponse(Call<List<model>> call, Response<List<model>> response) {
                 if(response.isSuccessful() &&response.body()!=null)
                 {
+                    progressDialog.dismiss();
                         for(int i=0;i<response.body().size();i++)
                         {
                             Toast.makeText(getApplicationContext(), ""+response.body().get(i).getCourseName(), Toast.LENGTH_SHORT).show();
